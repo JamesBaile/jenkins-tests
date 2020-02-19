93780533db7b02ce9600e575e415fdcb240ff667
Mikalai Yakauleu <mikalai.yakauleu@gpggroup.co.uk>
2020-02-19 11:04:29 +0300

    fix Jenkinsfile

--- src/main/java/JenkinsSandbox.java
+++ src/main/java/JenkinsSandbox.java
@@ -1,5 +1,7 @@
 public class JenkinsSandbox {
     public static void main(String[] args) {
+        System.out.println("========================");
         System.out.println("Jenkins + k8s = One love");
+        System.out.println("========================");
     }
 }
